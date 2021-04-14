450ddf1edb4e3ff758938da8a352e8affc9251d4
Francesca Salvini <franci98sp@gmail.com>
2021-04-14 16:36:37 +0200

    completato primo esercizio

--- script.js
+++ script.js
@@ -12,3 +12,55 @@
 // Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e 
 // falli subiti e stampiamo tutto in console.
 
+// PRIMO ESERCIZIO
+
+const arrayBici = [
+    {
+        "nome" : "Alpina",
+        "peso" : 10
+    },
+    {
+        "nome" : "Atena",
+        "peso" : 8
+    },
+    {
+        "nome" : "Breezer",
+        "peso" : 9
+    },
+    {
+        "nome" : "Challenge",
+        "peso" : 12
+    },
+    {
+        "nome" : "Endura",
+        "peso" : 6
+    },
+    {
+        "nome" : "Fox",
+        "peso" : 11
+    }
+];
+
+
+
+let biciclettaPiuLeggera = arrayBici[0];
+
+for (let i = 0; i < arrayBici.length; i++) {
+    let thisBici = arrayBici[i];
+    const {nome, peso} = thisBici;
+    console.log(nome, peso);
+    
+
+    if( thisBici.peso < biciclettaPiuLeggera.peso) {
+        biciclettaPiuLeggera = thisBici;
+    }
+} 
+
+console.log(
+    `
+    ${biciclettaPiuLeggera.nome} è la bici più leggera
+    `
+);
+
+
+// FINE PRIMO ESERCIZIO
